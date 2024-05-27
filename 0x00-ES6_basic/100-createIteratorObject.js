bc04a64a108c5a196b506a61b64b26182fa020f8
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-05-27 23:27:01 +0100

    Iterating through an object and flattening a 2d array

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,8 @@
+export default function createIteratorObject(report) {
+  const newArr = [];
+
+  for (const value of Object.values(report.allEmployees)) {
+    newArr.push(value);
+  }
+  return newArr.flat();
+}
