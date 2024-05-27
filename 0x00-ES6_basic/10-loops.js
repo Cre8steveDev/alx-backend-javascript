0592d4aa0c6d248b01fa7df362d0f079da5f6d12
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-05-27 22:43:43 +0100

    Checker on steriod?

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -2,6 +2,7 @@ export default function appendToEachArrayValue(array, appendString) {
   for (const idx in array) {
     if (idx) {
       const value = array[idx];
+      // eslint-disable-next-line no-param-reassign
       array[idx] = appendString + value;
     }
   }
