46431439862aab210828d26848e0f6fce8bab28f
Stephen Omoregie (Win) <cr8stevestudios@gmail.com>
2024-05-29 18:38:12 +0100

    fixed checker problem

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils';
 export default async function asyncUploadUser() {
   try {
     const data = await Promise.all([uploadPhoto(), createUser()]);
-    return data;
+    return { photo: data[0], user: data[1] };
   } catch (error) {
     return { photo: null, user: null };
   }
