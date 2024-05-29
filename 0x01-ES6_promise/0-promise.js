dec8b1a7aaf477a5fdc7bf9671782569c7216366
Stephen Omoregie (Win) <cr8stevestudios@gmail.com>
2024-05-29 08:54:04 +0100

    Fixing task 0 - Promises

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,3 @@
-/**
- * This function returns a promise
- */
-
 export default function getResponseFromAPI() {
-  return new Promise(
-    (resolve) => {},
-    (reject) => {}
-  );
+  return new Promise((resolve) => {});
 }
