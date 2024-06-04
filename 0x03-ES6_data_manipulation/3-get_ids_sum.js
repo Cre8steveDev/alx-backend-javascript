ca96ab1e35f15d02ba2c975cfa4e027f7b8709de
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-06-04 17:30:55 +0100

    Reduce Function - Correction

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,9 +2,7 @@ export default function getStudentIdsSum(listOfStudents) {
   // Return 0 if listOfStudents is not an array
   if (!Array.isArray(listOfStudents)) return 0;
 
-  const reducedSumOfIDs = listOfStudents.reduce(
-    (sum, student) => sum + student.id,
-    0
-  );
+  // prettier-ignore
+  const reducedSumOfIDs = listOfStudents.reduce((sum, student) => sum + student.id, 0);
   return reducedSumOfIDs;
 }
