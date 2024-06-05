c870a78eedbd574021f7d9e8fb4f64e10bc978a6
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-06-05 20:01:35 +0100

    Doing something with String

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   //   if (startString === '') return '';
   const myStr = [];
   for (const item of set) {
-    if (item.startsWith(startString)) {
+    if (item.startsWith(startString) && startString !== '') {
       let newStr = item;
       newStr = newStr.replaceAll(startString, '');
       myStr.push(newStr);
