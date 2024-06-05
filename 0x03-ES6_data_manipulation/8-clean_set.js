f9894274ab5b6f161e722848af2deee50aa55341
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-06-05 20:04:08 +0100

    Checking what goes on

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,23 @@
-export default function cleanSet(set, startString) {
-  //   if (startString === '') return '';
-  const myStr = [];
-  for (const item of set) {
-    if (item.startsWith(startString) && startString !== '') {
-      let newStr = item;
-      newStr = newStr.replaceAll(startString, '');
-      myStr.push(newStr);
-    }
-  }
+// export default function cleanSet(set, startString) {
+//   //   if (startString === '') return '';
+//   const myStr = [];
+//   for (const item of set) {
+//     if (item.startsWith(startString) && startString !== '') {
+//       let newStr = item;
+//       newStr = newStr.replaceAll(startString, '');
+//       myStr.push(newStr);
+//     }
+//   }
 
-  return myStr.join('-');
+//   return myStr.join('-');
+// }
+/* eslint-disable array-callback-return */
+export default function cleanSet(set, string) {
+  if (string === undefined || string.length === 0) {
+    return '';
+  }
+  return [...set]
+    .filter((str) => (str !== undefined ? str.startsWith(string) : ''))
+    .map((str) => (str !== undefined ? str.slice(string.length) : ''))
+    .join('-');
 }
