2b28bb2401c4be3b30a086831c1929cf17697e0e
Stephen Omoregie <cr8stevestudios@gmail.com>
2024-06-05 19:17:41 +0100

    Clean Set - ish

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,13 @@
+export default function cleanSet(set, startString) {
+  if (startString === '') return '';
+  const myStr = [];
+  for (const item of set) {
+    if (item.startsWith(startString)) {
+      let newStr = item;
+      newStr = newStr.replaceAll(startString, '');
+      myStr.push(newStr);
+    }
+  }
+
+  return myStr.join('-');
+}
---
+++ 0x03-ES6_data_manipulation/8-main.js
@@ -0,0 +1,8 @@
+/* eslint-disable import/extensions */
+import cleanSet from './8-clean_set.js';
+
+// prettier-ignore
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), 'bon'));
+
+// prettier-ignore
+console.log(cleanSet(new Set(['bonjovi', 'bonaparte', 'bonappetit', 'banana']), ''));
